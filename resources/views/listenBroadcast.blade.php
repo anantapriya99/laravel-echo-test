15c6f6e5489622840c7abf6e23d41df08403def0
anantapriya99 <anantapriya@graduate.utm.my>
2022-11-10 14:54:44 +0800

    first commit

---
+++ resources/views/listenBroadcast.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{csrf_token() }}">
+    <title>Document</title>
+
+</head>
+<body>
+
+    </div>
+
+</body>
+</html>    
